1a419940e5bdd199e67d06699ef3907fdba9ac63
Matthew Hibner <matthewhibner3@gmail.com>
2020-01-29 23:24:31 -0800

    create search page, added search function, added athlete details page, created viewmodel for details page, added search page link to navbar

---
+++ Milestones/M3/AthleteDataTrackerv2/Models/AthleteDataViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AthleteDataTrackerv2.Models
+{
+    public class AthleteDataViewModel
+    {
+        public AthleteDataViewModel(Athlete athlete, AthleteResult[] results)
+            {
+            FName = athlete.FName;
+            LName = athlete.LName;
+            Gender = athlete.Gender;
+            Results = results;
+            }
+
+        [Required]
+        [StringLength(50)]
+        public string FName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Gender { get; set; }
+
+        public AthleteResult[] Results { get; set; }
+    }
+}
