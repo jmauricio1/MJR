d00bfe9e109b8e1cd8e2395e65f6ee8d14eaf800
Joshua Jacob Mauricio (Laptop) <jmauricio15@wou.edu>
2020-02-12 17:57:20 -0800

    Adding the initial files for Astronomical Learning

---
+++ SeniorProject/Astronomical-Learning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Astronomical_Learning.Startup))]
+namespace Astronomical_Learning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
