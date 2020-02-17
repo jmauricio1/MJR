5398c9b68f6c026b1f62bb2e1852915cf3ab31f0
Matthew Hibner <matthewhibner3@gmail.com>
2020-02-16 22:15:47 -0800

    create sun page, added information, added references

---
+++ SeniorProject/Astronomical-Learning/Controllers/SolarSystemController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Astronomical_Learning.Controllers
+{
+    public class SolarSystemController : Controller
+    {
+        // GET: SolarSystem
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sun_Information()
+        {
+            return View();
+        }
+
+    }
+}
