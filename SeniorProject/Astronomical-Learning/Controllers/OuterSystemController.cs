1420442c7e72f33bd803b7e1f132110a6d70f012
Matthew Hibner <matthewhibner3@gmail.com>
2020-02-18 22:43:37 -0800

    create outer system controller, added outer system section to nav bar, added andromeda page to home page, created andromeda information page, addeed information and references to andromeda page.

---
+++ SeniorProject/Astronomical-Learning/Controllers/OuterSystemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Astronomical_Learning.Controllers
+{
+    public class OuterSystemController : Controller
+    {
+        // GET: OuterSystem
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Andromeda_Information()
+        {
+            return View();
+        }
+    }
+}
