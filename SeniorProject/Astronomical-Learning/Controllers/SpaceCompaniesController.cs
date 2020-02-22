594f501846c507b82962f023b73b48385efe2732
Robert Lochbaum-DSKTP <rlochbaum16@mail.wou.edu>
2020-02-22 02:34:37 -0800

    About NASA information complete

--- SeniorProject/Astronomical-Learning/Controllers/SpaceCompaniesController.cs
+++ SeniorProject/Astronomical-Learning/Controllers/SpaceCompaniesController.cs
@@ -13,5 +13,10 @@ namespace Astronomical_Learning.Controllers
         {
             return View();
         }
+
+        public ActionResult NASA()
+        {
+            return View();
+        }
     }
 }
