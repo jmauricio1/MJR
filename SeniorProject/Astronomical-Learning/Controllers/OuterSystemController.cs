eae1af7d40ad34fa2d16dabd1fbc4906c76ea3b6
Matthew Hibner <matthewhibner3@gmail.com>
2020-02-24 00:42:41 -0800

    hopefully fixed project

---
+++ SeniorProject/Astronomical-Learning/Controllers/OuterSystemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Astronomical_Learning.Controllers
+{
+    public class OuterSystemController : Controller
+    {
+        // GET: OuterSystem
+        public ActionResult MilkyWay_Information()
+        {
+            return View();
+        }
+
+        public ActionResult Andromeda_Information()
+        {
+            return View();
+        }
+    }
+}
---
+++ SeniorProject/Astronomical-Learning/Controllers/SolarSystemController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Astronomical_Learning.Controllers
+{
+    public class SolarSystemController : Controller
+    {
+        // GET: SolarSystem
+        public ActionResult Moon_Information()
+        {
+            return View();
+        }
+
+        public ActionResult Sun_Information()
+        {
+            return View();
+        }
+
+        public ActionResult SpaceDebris_Information()
+        {
+            return View();
+        }
+
+        public ActionResult Our_Planets()
+        {
+            return View();
+        }
+    }
+}
