04d8263ef13ef4f6577ab6408f858bf7897b17cf
Joshua Jacob Mauricio (Laptop) <jmauricio15@wou.edu>
2020-02-29 15:54:15 -0800

    Things needed for continuous deployment

---
+++ SeniorProject/Astronomical-Learning/Models/Launch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Astronomical_Learning.Models
+{
+    public class Launch
+    {
+        public string missionName;
+        public string missionDate;
+    }
+}
