fe2db3dc0fff4cddd8d870f0be7b8aea2242d94d
Matthew Hibner <matthewhibner3@gmail.com>
2020-02-29 03:19:52 -0800

    added solar flare/wind pictures, added descriptions under the pictures along with the sources

---
+++ SeniorProject/Astronomical-Learning/Scripts/main.js
@@ -0,0 +1,17 @@
+﻿function sunShowImageSources() {
+    var x = document.getElementById("sunSources");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function sunShowFunFacts() {
+    var x = document.getElementById("sunFunFacts");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
