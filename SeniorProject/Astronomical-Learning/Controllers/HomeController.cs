79619f6217a6639c645413f6ce68ff5bec273140
Matthew Hibner <matthewhibner3@gmail.com>
2020-03-30 18:08:39 -0700

    added comments to code

--- SeniorProject/Astronomical-Learning/Controllers/HomeController.cs
+++ SeniorProject/Astronomical-Learning/Controllers/HomeController.cs
@@ -14,36 +14,35 @@ namespace Astronomical_Learning.Controllers
     {
         public ActionResult Index()
         {
+            //get the picture of the day key from the web config
             string apodKey = ConfigurationManager.AppSettings["APOD_Key"]; ;
 
+            //get the data using the key and parse the data that is returned
             string pictureOfTheDayData = getAPOD(apodKey);
             JObject potdData = JObject.Parse(pictureOfTheDayData);
 
-
-
+            //get the information from the data
             ViewBag.pictureUrl = potdData["url"];
             ViewBag.pictureTitle = potdData["title"];
             
+            //get the information and remove the end that is not relevant
             string explanation = (string)potdData["explanation"];
             explanation = explanation.Remove(explanation.Length - "Activities: NASA Science at Home".Length);
             ViewBag.pictureExplanation = explanation;
 
-
             return View();
         }
 
-
+        //the method the gets the nasa picture of the day information using the web config key
         private string getAPOD(string key)
         {
+            //create the url and request the information
             string url = "https://api.nasa.gov/planetary/apod?api_key=";
-
             url = url + key;
-            
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             
-
+            //read in the information
             string jsonString = null;
-            // TODO: You should handle exceptions here
             using (WebResponse response = request.GetResponse())
             {
                 Stream stream = response.GetResponseStream();
@@ -52,6 +51,7 @@ namespace Astronomical_Learning.Controllers
                 reader.Close();
                 stream.Close();
             }
+
             return jsonString;
         }
 
