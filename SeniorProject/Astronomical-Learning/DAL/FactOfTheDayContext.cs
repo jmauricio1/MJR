e210979e137b3361be34d0045f9a5dc6507c6280
Matthew Hibner <matthewhibner3@gmail.com>
2020-04-08 00:04:10 -0700

    added up down and seed scripts, changed alcontext to include fact of the day table, changed homepage to include fact of the day in the information of the day section, changed home controller to include a database and added code to get a fact from the database to the action result

---
+++ SeniorProject/Astronomical-Learning/DAL/FactOfTheDayContext.cs
@@ -0,0 +1,21 @@
+namespace Astronomical_Learning.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class FactOfTheDayContext : DbContext
+    {
+        public FactOfTheDayContext()
+            : base("name=ALContext")
+        {
+        }
+
+        public virtual DbSet<FactOfTheDay> FactOfTheDays { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
