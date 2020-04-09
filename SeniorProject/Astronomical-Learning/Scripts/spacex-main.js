c5454134cd9c0376d5ae3e61ae6dd1e45e546762
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-04-09 15:00:10 -0700

    minor changes to the home and list of launches pages

--- SeniorProject/Astronomical-Learning/Scripts/spacex-main.js
+++ SeniorProject/Astronomical-Learning/Scripts/spacex-main.js
@@ -15,26 +15,26 @@ function errorOnAjax() {
 function displaySpaceXList(data) {
     //console.log(data);
 
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 1; i < data.length + 1; i++) {
         var module = document.createElement('div');
-        module.className = "col-lg-4 col-md-6 col-sm-12 col-xs-12 launch-module";
+        module.className = "col-lg-4 col-md-6 col-sm-12 col-xs-12 launch-module text-center";
 
         var missname = document.createElement('h3');
-        var missnameText = document.createTextNode(data[i]["missionName"]);
+        var missnameText = document.createTextNode(data[i-1]["missionName"]);
         missname.className = "text-center";
         missname.append(missnameText);
 
         var missdate = document.createElement('h4');
-        missdateText = document.createTextNode(data[i]["missionDate"]);
+        missdateText = document.createTextNode(data[i-1]["missionDate"]);
         missdate.className = "text-center";
         missdate.append(missdateText);
 
         var success = document.createElement('h4');
-        if (data[i]["launchSuccess"] == "True") {
+        if (data[i-1]["launchSuccess"] == "True") {
             success.append("Successful");
             success.className = "text-center success-text";
         }
-        else if (data[i]["launchSuccess"] == null) {
+        else if (data[i-1]["launchSuccess"] == null) {
             success.append("Not Yet Launched");
             success.className = "text-center null-text";
         }
@@ -46,13 +46,19 @@ function displaySpaceXList(data) {
         var button = document.createElement('a');
         button.append("Details");
         button.className = "btn btn-primary";
-        button.href = '/SpaceCompanies/LaunchDetails?id=' + data[i]["flightNum"];
+        button.href = '/SpaceCompanies/LaunchDetails?id=' + data[i-1]["flightNum"];
+
+        var sameLine = document.createElement('div');
+        sameLine.className = "clearfix"
 
         module.appendChild(missname);
         module.appendChild(missdate);
         module.appendChild(success);
         module.appendChild(button);
-
+        
         $('#launch-list').append(module);
+        if (((i % 3) == 0)) {
+            $('#launch-list').append(sameLine);
+        }
     }
 }
