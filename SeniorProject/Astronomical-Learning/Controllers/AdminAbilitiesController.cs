f7dc0bb63010c6589c84b50a7478228b5cc41c8e
Matthew Hibner <matthewhibner3@gmail.com>
2020-05-03 18:29:30 -0700

    added area for super admin abilites to profile page, created lists for admins and banned admins, made new page to edit admin bans, added page to create new admins, edited user and banned users lists to only get users, made ability to create new admins

--- SeniorProject/Astronomical-Learning/Controllers/AdminAbilitiesController.cs
+++ SeniorProject/Astronomical-Learning/Controllers/AdminAbilitiesController.cs
@@ -4,11 +4,13 @@ using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Astronomical_Learning.DAL;
 using Astronomical_Learning.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace Astronomical_Learning.Controllers
 {
@@ -150,7 +152,18 @@ namespace Astronomical_Learning.Controllers
             }
 
 
-            var bannedUsers = db.AspNetUsers.Where(x => x.LockoutEndDateUtc.HasValue);
+            var allBanned = db.AspNetUsers.Where(x => x.LockoutEndDateUtc.HasValue).ToArray();
+
+            List<DAL.AspNetUser> bannedUsers = new List<DAL.AspNetUser>();
+
+
+            for (int i = 0; i < allBanned.Length; i++)
+            {
+                if (allBanned[i].AspNetRoles.ElementAt(0).Id == "US")
+                {
+                    bannedUsers.Add(allBanned[i]);
+                }
+            }
 
             return View(bannedUsers);
         }
@@ -158,8 +171,21 @@ namespace Astronomical_Learning.Controllers
         [HttpPost]
         public ActionResult BannedUsers(string searchInput)
         {
-            var searchedUsers = db.AspNetUsers.Where(y => y.LockoutEndDateUtc.HasValue).Where(x => x.UserName.Contains(searchInput));
-            return View(searchedUsers);
+            var searchedUsers = db.AspNetUsers.Where(y => y.LockoutEndDateUtc.HasValue).Where(x => x.UserName.Contains(searchInput)).ToArray();
+
+            List<DAL.AspNetUser> bannedUsers = new List<DAL.AspNetUser>();
+
+
+            for (int i = 0; i < searchedUsers.Length; i++)
+            {
+                if (searchedUsers[i].AspNetRoles.ElementAt(0).Id == "US")
+                {
+                    bannedUsers.Add(searchedUsers[i]);
+                }
+            }
+
+
+            return View(bannedUsers);
         }
 
 
@@ -218,6 +244,264 @@ namespace Astronomical_Learning.Controllers
 
             return role;
         }
+
+
+        //////////////////////////////////////////////
+        /// start of super user only abilities
+        ////////////////////////////////////////////////////////
+
+
+
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult AdminCreate()
+        {
+            
+            return View();
+        }
+
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AdminCreate(RegisterViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Random rand = new Random();
+                int x = rand.Next(1, 7);
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName, Country = model.Country, StateProvince = model.StateProvince, AID = x };
+                if (db.AspNetUsers.Any(m => m.UserName == user.UserName) == true)
+                {
+                    ViewBag.UsernameTaken = "Username already taken or unavailable.";
+                    return View();
+                }
+                else
+                {
+                    var result = await UserManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
+                    {
+                        UserManager.AddToRole(user.Id, "Administrator");
+
+                        db.SaveChanges();
+
+                        return RedirectToAction("AdminCreate", "AdminAbilities");
+                    }
+                    AddErrors(result);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+
+
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        public ActionResult AllAdmin()
+        {
+
+
+            if (User.Identity.Name == "")
+            {
+                return Redirect("~/Home/Index");
+            }
+
+            string role = checkUserRole();
+
+            if (role == "US" || role == "AD")
+            {
+                return Redirect("~/Home/Index");
+            }
+
+
+
+            var allUsers = db.AspNetUsers.ToArray();
+
+
+            List<DAL.AspNetUser> allAdmins = new List<DAL.AspNetUser>();
+
+
+            for (int i = 0; i < allUsers.Length; i++)
+            {
+                if (allUsers[i].AspNetRoles.ElementAt(0).Id == "AD")
+                {
+                    allAdmins.Add(allUsers[i]);
+                }
+            }
+
+
+            return View(allAdmins);
+
+        }
+
+        [HttpPost]
+        public ActionResult AllAdmin(string searchInput)
+        {
+
+
+
+            var searchedUsers = db.AspNetUsers.Where(x => x.UserName.Contains(searchInput)).ToArray();
+
+            List<DAL.AspNetUser> searchedAdmins = new List<DAL.AspNetUser>();
+
+
+            for (int i = 0; i < searchedUsers.Length; i++)
+            {
+                if (searchedUsers[i].AspNetRoles.ElementAt(0).Id == "AD")
+                {
+                    searchedAdmins.Add(searchedUsers[i]);
+                }
+            }
+
+
+            return View(searchedAdmins);
+        }
+
+        public ActionResult BannedAdmin()
+        {
+
+
+
+            if (User.Identity.Name == "")
+            {
+                return Redirect("~/Home/Index");
+            }
+
+            string role = checkUserRole();
+
+            if (role == "US"|| role == "AD")
+            {
+                return Redirect("~/Home/Index");
+            }
+
+
+            var allBanned = db.AspNetUsers.Where(x => x.LockoutEndDateUtc.HasValue).ToArray();
+
+            List<DAL.AspNetUser> bannedAdmins = new List<DAL.AspNetUser>();
+
+            for (int i = 0; i < allBanned.Length; i++)
+            {
+                if (allBanned[i].AspNetRoles.ElementAt(0).Id == "AD")
+                {
+                    bannedAdmins.Add(allBanned[i]);
+                }
+            }
+
+            return View(bannedAdmins);
+        }
+
+        [HttpPost]
+        public ActionResult BannedAdmin(string searchInput)
+        {
+            var searchedUsers = db.AspNetUsers.Where(y => y.LockoutEndDateUtc.HasValue).Where(x => x.UserName.Contains(searchInput)).ToArray();
+
+            List<DAL.AspNetUser> bannedAdmins = new List<DAL.AspNetUser>();
+
+            for (int i = 0; i < searchedUsers.Length; i++)
+            {
+                if (searchedUsers[i].AspNetRoles.ElementAt(0).Id == "AD")
+                {
+                    bannedAdmins.Add(searchedUsers[i]);
+                }
+            }
+
+
+            return View(searchedUsers);
+        }
+
+
+
+
+        public ActionResult EditAdminBan(string id)
+        {
+
+
+            if (User.Identity.Name == "")
+            {
+                return Redirect("~/Home/Index");
+            }
+
+            string role = checkUserRole();
+
+            if (role == "US" || role == "AD")
+            {
+                return Redirect("~/Home/Index");
+            }
+
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DAL.AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AID = new SelectList(db.AvatarPaths, "ID", "AvatarName", aspNetUser.AID);
+            return View(aspNetUser);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditAdminBan(DAL.AspNetUser aspNetUser)
+        {
+
+            var user = db.AspNetUsers.Find(aspNetUser.Id);
+            user.LockoutEndDateUtc = aspNetUser.LockoutEndDateUtc;
+
+
+            db.SaveChanges();
+            //return View(user);
+
+
+            return RedirectToAction("AllAdmin");
+        }
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
