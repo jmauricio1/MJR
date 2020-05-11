d6a254d03b5efcb3f1926972dd8afe78781f4c71
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-05-11 00:18:59 -0700

    minor changes after finding minor problems on deployed site

--- SeniorProject/Astronomical-Learning/App_Data/FactOfTheDay/facts-update.sql
+++ SeniorProject/Astronomical-Learning/App_Data/FactOfTheDay/facts-update.sql
@@ -5,3 +5,11 @@
 
 ALTER TABLE [dbo].[FactOfTheDay] ADD
 	[LastDisplayed] DATETIME	NULL;
+
+ALTER TABLE [dbo].[FactOfTheDay] DROP
+	COLUMN [DateSubmitted],
+	[LastDisplayed];
+
+ALTER TABLE [dbo].[FactOfTheDay] ADD
+	[DateSubmitted]	DATETIME2	  NULL,
+	[LastDisplayed] DATETIME2	NULL;
