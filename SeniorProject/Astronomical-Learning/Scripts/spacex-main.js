93688ab274eb765532fcafa0f798ee4331192e5c
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-05-18 22:18:32 -0700

    Making sure that the idea of this works before implementing the rest

--- SeniorProject/Astronomical-Learning/Scripts/spacex-main.js
+++ SeniorProject/Astronomical-Learning/Scripts/spacex-main.js
@@ -12,6 +12,44 @@ function errorOnAjax() {
     console.log("ERROR on ajax request.");
 }
 
+$('#launchSuccess-dropdown').change(function () {
+    var launch = $('#launchSuccess-dropdown').val();
+    var land = $('#landSuccess-dropdown').val();
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "/SpaceCompanies/SearchDefined",
+        data: {
+            launchSuccess: launch,
+            landSuccess: land
+        },
+        success: work,
+        error: errorOnAjax
+    });
+});
+
+$('#landSuccess-dropdown').change(function () {
+    var launch = $('#launchSuccess-dropdown').val();
+    var land = $('#landSuccess-dropdown').val();
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "/SpaceCompanies/SearchDefined",
+        data: {
+            launchSuccess: launch,
+            landSuccess: land
+        },
+        success: work,
+        error: errorOnAjax
+    });
+});
+
+function work(data) {
+    console.log(data);
+}
+
 function displaySpaceXList(data) {
     //console.log(data);
 
@@ -20,21 +58,21 @@ function displaySpaceXList(data) {
         module.className = "col-lg-4 col-md-6 col-sm-12 col-xs-12 launch-module text-center";
 
         var missname = document.createElement('h3');
-        var missnameText = document.createTextNode(data[i-1]["missionName"]);
+        var missnameText = document.createTextNode(data[i - 1]["missionName"]);
         missname.className = "text-center";
         missname.append(missnameText);
 
         var missdate = document.createElement('h4');
-        missdateText = document.createTextNode(data[i-1]["missionDate"]);
+        missdateText = document.createTextNode(data[i - 1]["missionDate"]);
         missdate.className = "text-center";
         missdate.append(missdateText);
 
         var success = document.createElement('h4');
-        if (data[i-1]["launchSuccess"] == "True") {
+        if (data[i - 1]["launchSuccess"] == "True") {
             success.append("Successful");
             success.className = "text-center success-text";
         }
-        else if (data[i-1]["launchSuccess"] == null) {
+        else if (data[i - 1]["launchSuccess"] == null) {
             success.append("Not Yet Launched");
             success.className = "text-center null-text";
         }
@@ -46,7 +84,7 @@ function displaySpaceXList(data) {
         var button = document.createElement('a');
         button.append("Details");
         button.className = "btn btn-primary";
-        button.href = '/SpaceCompanies/LaunchDetails?id=' + data[i-1]["flightNum"];
+        button.href = '/SpaceCompanies/LaunchDetails?id=' + data[i - 1]["flightNum"];
 
         var sameLine = document.createElement('div');
         sameLine.className = "clearfix"
@@ -55,7 +93,7 @@ function displaySpaceXList(data) {
         module.appendChild(missdate);
         module.appendChild(success);
         module.appendChild(button);
-        
+
         $('#launch-list').append(module);
         if (((i % 3) == 0)) {
             $('#launch-list').append(sameLine);
