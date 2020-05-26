a98d861a5aa723c7538796363527e36e04a18765
Matthew Hibner <matthewhibner3@gmail.com>
2020-05-26 01:03:26 -0700

    added new projects controller, added pages to view and create projects, added admin page to reivew projects, added js fucntions to accept or delete projects, added section to admin functions page, added projects to navbar and edited navbar to fit them, moved several js functions to different files and edited the needed pages to include them, added up down and model for projects table, eedited context to include projects table

---
+++ SeniorProject/Astronomical-Learning/Scripts/adminFunctions.js
@@ -0,0 +1,33 @@
+﻿function acceptComment(commentId) {
+    var accept = confirm("Are you sure you want to accept this comment? All users will be able to see this comment after it is acccepted.");
+
+    if (accept == true) {
+        $.post("/AdminAbilities/AcceptComment?commentId=" + commentId, commentId, acceptCommentSuccess(), 'int');
+    }
+}
+
+function acceptCommentSuccess() {
+    alert("This comment has been accepted.");
+
+    setTimeout(function () {
+        location.reload();
+    }, 350);
+}
+
+
+
+function acceptProject(projectId) {
+    var accept = confirm("Are you sure you want to accept this project? All users will be able to see this project after it is acccepted.");
+
+    if (accept == true) {
+        $.post("/AdminAbilities/AcceptProject?projectId=" + projectId, projectId, acceptProjectSuccess(), 'int');
+    }
+}
+
+function acceptProjectSuccess() {
+    alert("This project has been accepted.");
+
+    setTimeout(function () {
+        location.reload();
+    }, 350);
+}
