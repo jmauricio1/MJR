fb40f2c938c3fa38587e79067136cbda830f7573
Robert Lochbaum-DSKTP <rlochbaum16@mail.wou.edu>
2020-05-31 21:44:52 -0700

    Feature complete

---
+++ SeniorProject/Astronomical-Learning/App_Data/ViewsData/seedViewData.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[ViewData] (PageName, ViewCount)
+	VALUES ('Sun', 1), ('Moon', 1), ('Planets', 1), ('Mars', 1), ('SpaceDebris', 1),
+		   ('MilkyWay', 1), ('Andromeda', 1), ('Stars', 1);
