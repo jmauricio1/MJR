f2a72d56e3698896b4cce30e51956d90c44e6752
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-06-02 16:48:45 -0700

    Added ViewsData scripts to upscript and removed it from App_Data

--- SeniorProject/Astronomical-Learning/App_Data/ViewsData/upViewData.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[ViewData]
-(
-    [Id]            INT             IDENTITY(1,1)   NOT NULL,
-    [PageName]      NVARCHAR        (64)            NOT NULL,
-    [ViewCount]     DATE                            NOT NULL
-
-    CONSTRAINT [PK_dbo.ViewData] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
