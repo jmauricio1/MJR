d5c0a399eaff2d9b253d0e684438929bc3f0f3b8
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-06-02 16:43:02 -0700

    Removing scripts that have been included with the updated up and down sql script

--- SeniorProject/Astronomical-Learning/App_Data/UserComment/demoSeedScript.sql
+++
@@ -1,3 +0,0 @@
-﻿INSERT INTO [dbo].[UserComment] (Username, PostDate, PageFrom, AcceptState, Comment, ReportCount)
-	VALUES ('TestUser1', '1/1/2020', '/SolarSystem/Sun_Information', 1, 'This is a good comment', 0),
-		   ('TestUser4Reports', '1/1/2020', '/SolarSystem/Sun_Information', 1, 'This is a malicious content', 4);
